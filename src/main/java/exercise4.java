10fe9691bbebaf5407383bb3c6ba6d0edbb069f4
guoyu <hahahahaokaixin@gmail.com>
2017-02-27 21:31:43 +0800

    更新

--- src/main/java/exercise4.java
+++ src/main/java/exercise4.java
@@ -9,29 +9,30 @@
 public class exercise4 {
     public static void main(String args[]){
         int []a={1,2,3,4,5,56,54,21,34};
-        bbb(a);
+        int []d=bbb(a);
+        aaa(d);
 
+        int []b={98,33,78};
+        int c[]=bbb(b);
+        bb(c);
+        aaa(c);
     }
     public static int []bbb(int a[]){
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a.length - 1; j++) {
-                if (a[j] > a[j + 1]) {
+                if (a[j] < a[j + 1]) {
                     int tmp = a[j];
                     a[j] = a[j + 1];
                     a[j + 1] = tmp;
                 }
-                System.out.println();
-                for (int t = 0; t < a.length; t++) {
-                    System.out.print(a[t] + " ");
-                }
-                System.out.println();
             }
-            for (int t = 0; t < a.length; t++) {
-                System.out.print(a[t] + " ");
-            }
-            System.out.println();
         }
         return a;
+    }
+    public static void aaa(int a[]){
+        for (int i=0;i<a.length;i++){
+            System.out.print(a[i]);
+        }
     }
     public static int [] bb(int a[]){
         for (int i=0;i<a.length;i++){
@@ -47,5 +48,6 @@ public class exercise4 {
         }
       return a;
     }
+
 
 }
